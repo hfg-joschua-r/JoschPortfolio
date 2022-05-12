e7aa9713be4bf1758e99dcd59a337d0ef39ff8b8
joschroth <joschua.rothenbacher@hfg.design>
2022-05-12 17:37:32 +0200

    changed project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,7 +6,7 @@
         <div class="absolute top-0 left-0 px-6 py-4 container h-full" v-if="showText1">
           <h4 class="mb-1 lg:text-2xl sm:text-xl text-lg tracking-tight text-white font-normal">WiWa - a wildlife crossing warning system</h4>
           <h5 class="primeHeader mb-1 sm:mb-3 lg:text-lg text-sm font-semibold tracking-tight">Prototyping * RaspberryPi * ReDesign </h5>
-          <p class="leading-normal sm:text-lg text-sm text-gray-100 descriptionText 2xl:w-3/5">WiWa is a sensor system attached to the delineator, which warns the driver both locally at danger spots and collects and analyses the data externally.</p>
+          <p class="leading-normal sm:text-lg text-sm text-gray-100 descriptionText 2xl:w-3/5">WiWa is a sensor system attached to the delineator, which warns the driver locally at danger spots and collects & analyses it externally.</p>
         <projectButton link="https://ausstellung.hfg-gmuend.de/w-2122/projekte/wiwa-ein-wildwechsel-warnsystem/studiengang:iot" title="Documentation & More" link2="https://youtu.be/rXBEbwPFTkE" title2="Video" class="lg:my-8 my-2"></projectButton>
   </div>
     </div>
@@ -31,11 +31,11 @@
     </div>
 
     <div class="2xl:col-span-1 2xl:row-span-1 col-span-2 transform transition duration-500 hover:scale-105 opacity-70 hover:opacity-100" v-on:click="showText4 = !showText4">
-      <img class="w-full rounded-lg" src=" .\images\tanzmitdemtiger.png" />
+      <img class="w-full rounded-lg" src=" .\images\etage.png" />
       <div class="absolute top-0 left-0 px-6 py-4 container h-full" v-if="showText4">
-          <h4 class="mb-1 lg:text-2xl sm:text-xl text-lg tracking-tight text-white font-normal">"Tanz mit dem Tiger" - AR Dance-Performance</h4>
-          <h5 class="primeHeader mb-1 sm:mb-3 lg:text-lg text-sm font-semibold tracking-tight">Unity * C# * AR</h5>
-          <p class="leading-normal sm:text-lg text-sm text-gray-100 descriptionText">In this project I supported the AR development in Unity for the dance performance. </p>
+          <h4 class="mb-1 lg:text-2xl sm:text-xl text-lg tracking-tight text-white font-normal">"Workshop instructor - e.tage</h4>
+          <h5 class="primeHeader mb-1 sm:mb-3 lg:text-lg text-sm font-semibold tracking-tight">Arduino * Teaching * Media education</h5>
+          <p class="leading-normal sm:text-lg text-sm text-gray-100 descriptionText">In the school holidays I run workshops teaching Arduino to 9-12 year olds. </p>
         <projectButton link="https://www.widerstandundsoehne.de/projekte/tanz-mit-dem-tiger" title="Learn more" link2="https://www.roxy.ulm.de/programm/programm.php?vid=3792" title2="Roxy Ulm" class="lg:my-6 my-2"></projectButton>
       </div>
     </div>
