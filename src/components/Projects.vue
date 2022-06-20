1cd7206895877a82219f080b671c61901d6bcc0e
joschroth <joschua.rothenbacher@hfg.design>
2022-06-20 14:42:32 +0200

    Changed Link in fishing projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -46,7 +46,7 @@
           <h4 class="mb-1 lg:text-2xl sm:text-xl text-lg tracking-tight text-white font-normal">Data Visualisation: Fishing & Fish consumption</h4>
           <h5 class="primeHeader mb-1 sm:mb-3 lg:text-lg text-sm font-semibold tracking-tight">p5.js * data * design principles</h5>
           <p class="leading-normal sm:text-lg text-sm text-gray-100 descriptionText">In the course of the data visualisation course @HfG, we developed an interactive representation of the global fish catch and related it to fish consumption.</p>
-        <projectButton link="https://portfolio.hfg-gmuend.de/s-2121/projekte/fischfang-fischkonsum" title="More info" link2="https://github.com/hfg-joschua-r/DarstellenSimulationFishing" title2="GitHub" class="lg:my-6 my-2"></projectButton>
+        <projectButton link="https://portfolio.hfg-gmuend.de/s-2121/projekte/fischfang-fischkonsum" title="More info" link2="https://hfg-joschua-r.github.io/DarstellenSimulationFishing/" title2="Demo" class="lg:my-6 my-2"></projectButton>
       </div>
     </div> 
   </div>
